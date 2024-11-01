16eb5771f556c51152e0cea467514e5ac9a74444
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-11-01 22:20:27 +0530

    adds usePrev custom hooks

--- react_playground/prac_rev_1/src/App.tsx
+++ react_playground/prac_rev_1/src/App.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import useFetch from "./hooks/useFetch";
+import { usePrev } from "./hooks/usePrev";
+
+// function App() {
+//   const { data } = useFetch("https://jsonplaceholder.typicode.com/posts");
+
+//   return (
+//     <>
+//       {data.map((post) => (
+//         <>
+//           <span id={post.id}>{post.title}</span>
+//           <br></br>
+//         </>
+//       ))}
+//     </>
+//   );
+// }
 
 function App() {
-  const { data } = useFetch("https://jsonplaceholder.typicode.com/posts");
+  const [count, setCount] = useState(0);
+  const prev = usePrev(count);
+
+  const increaseCount = () => {
+    setCount((c) => c + 1);
+  };
 
   return (
     <>
-      {data.map((post) => (
-        <>
-          <span id={post.id}>{post.title}</span>
-          <br></br>
-        </>
-      ))}
+      <p>Current state {count}</p>
+      <button onClick={increaseCount}>Click Me</button>
+      <p>Previous state {prev}</p>
     </>
   );
 }
