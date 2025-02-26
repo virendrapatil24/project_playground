cf8cc98be702cabfba8cbf4eb935b8f730dda4ae
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-02-26 22:18:59 +0530

    adds prisma handling

--- next_js_playground/todo/app/api/user/route.ts
+++ next_js_playground/todo/app/api/user/route.ts
@@ -1,7 +1,24 @@
+import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+
+const client = new PrismaClient();
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
 
-    return NextResponse.json({ username: body.username, password: body.password })
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
+}
+
+export async function GET() {
+    const user = await client.user.findFirst({});
+    return Response.json({ name: user?.username, email: user?.username })
 }
